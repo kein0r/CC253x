dc214d0596344e6cd89357d8fa765685964dc7bb
kein0r <github@dummgebabbel.de>
2014-09-29 03:34:21 +0000

    Changed type definition name from uint8 to uint8_t for 8, 16 and 32 bit variables

--- PlatformTypes.h
+++ PlatformTypes.h
@@ -19,14 +19,14 @@
 /*******************| Type definitions |*******************************/
 
 /* Data lenght according to EW8051_CompilerReference.pdf page 276 */
-typedef unsigned char uint8;
-typedef signed char sint8;
+typedef unsigned char uint8_t;
+typedef signed char sint8_t;
 
-typedef unsigned short uint16;
-typedef signed short sint16;
+typedef unsigned short uint16_t;
+typedef signed short sint16_t;
 
-typedef unsigned long uint32;
-typedef signed long sint32;
+typedef unsigned long uint32_t;
+typedef signed long sint32_t;
 
 /*******************| Global variables |*******************************/
 
