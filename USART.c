7682e475237b01110d64d59b2fb2c2e8cbec9eb8
kein0r <github@dummgebabbel.de>
2014-09-29 07:44:44 +0000

    Added init function for USART Added first version of global configuration variable

--- USART.c
+++ USART.c
@@ -2,10 +2,15 @@
  * @{
  */
 #include <USART.h>
+#include <board.h>
 
 /*******************| Inclusions |*************************************/
 
 /*******************| Macros |*****************************************/
+#if BOARD_CPUFREQUENCY != 32000000
+#error Board frequemcy must be set to 32MHz for UART to work properly
+#endif
+
 
 /*******************| Type definitions |*******************************/
 
@@ -20,8 +25,52 @@
  * baudrates settings will be ignored and default baudrate of 9600 will be
  * used instead.
 */
-void USART_init(uint16_t baudrate)
+void USART_init(USART_Baudrate_t baudrate)
 {
+  /* baudrate register values are taken from swru191c.pdf Table 17-1. Commonly 
+   * Used Baud-Rate Settings for 32 MHz System Clock */
+  switch (baudrate)
+  {
+    case USART_Baudrate_14400:
+      U0BAUD = 216;
+      U0GCR = 8 & REGISTER_MASK_UxGCR_BAUD_E;
+      break;
+    case USART_Baudrate_19200:
+      U0BAUD = 59;
+      U0GCR = 9 & REGISTER_MASK_UxGCR_BAUD_E;
+      break;
+    case USART_Baudrate_28800:
+      U0BAUD = 216;
+      U0GCR = 9 & REGISTER_MASK_UxGCR_BAUD_E;
+      break;
+    case USART_Baudrate_38400:
+      U0BAUD = 59;
+      U0GCR = 10 & REGISTER_MASK_UxGCR_BAUD_E;
+      break;
+    case USART_Baudrate_57600:
+      U0BAUD = 216;
+      U0GCR = 10 & REGISTER_MASK_UxGCR_BAUD_E;
+      break;
+    case USART_Baudrate_76800:
+      U0BAUD = 59;
+      U0GCR = 11 & REGISTER_MASK_UxGCR_BAUD_E;
+      break;
+    case USART_Baudrate_115200:
+      U0BAUD = 216;
+      U0GCR = 11 & REGISTER_MASK_UxGCR_BAUD_E;
+      break;
+    case USART_Baudrate_230400:
+      U0BAUD = 216;
+      U0GCR = 12 & REGISTER_MASK_UxGCR_BAUD_E;
+      break;
+    default:    /* defaults to 9600 or USART_BAUDRATE_DEFAULT */
+      U0BAUD = 59;
+      U0GCR = 8 & REGISTER_MASK_UxGCR_BAUD_E; 
+  }
+  /* Set U0CSR register to USART, receive enable */
+  U0CSR = 0x00;
+  /* set transmission mode to 8N1, low level at start and stop, flush USART module */
+  U0UCR = USART_U0UCR_FLUSH | USART_U0UCR_D9_ODD | USART_U0UCR_BIT9_DISABLE | USART_U0UCR_PARITY_DISABLE | USART_U0UCR_SPB_1BIT | USART_U0UCR_STOP_LOW | USART_U0UCR_START_LOW;
 }
 
 /** @}*/
--- USART.h
+++ USART.h
@@ -8,14 +8,45 @@
 #include <PlatformTypes.h>
    
 /*******************| Macros |*****************************************/
-#define USART_BAUDRATE_DEFAULT  (uint16_t)9600
+/** Do not change this value! It will be used internally. Default baudrate
+ * will always be 9600 baud. Equally of the value set here */
+#define USART_BAUDRATE_DEFAULT  USART_Baudrate_9600
+   
+#define REGISTER_MASK_UxGCR_BAUD_E      0x1f
+   
+#define USART_U0UCR_FLUSH               0x80
+#define USART_U0UCR_ENABLE_FLOW_DISABLE 0x40
+#define USART_U0UCR_ENABLE_FLOW_ENABLE  0x40
+#define USART_U0UCR_D9_ODD              0x00
+#define USART_U0UCR_D9_EVEN             0x20
+#define USART_U0UCR_BIT9_DISABLE        0x00
+#define USART_U0UCR_BIT9_ENABLE         0x10
+#define USART_U0UCR_PARITY_DISABLE      0x00
+#define USART_U0UCR_PARITY_ENABLE       0x08
+#define USART_U0UCR_SPB_1BIT            0x00
+#define USART_U0UCR_SPB_2BIT            0x04
+#define USART_U0UCR_STOP_LOW            0x00
+#define USART_U0UCR_STOP_HIGH           0x02
+#define USART_U0UCR_START_LOW           0x00
+#define USART_U0UCR_START_HIGH          0x01
 
 /*******************| Type definitions |*******************************/
+typedef enum {
+  USART_Baudrate_9600,
+  USART_Baudrate_14400,
+  USART_Baudrate_19200,
+  USART_Baudrate_28800,
+  USART_Baudrate_38400,
+  USART_Baudrate_57600,
+  USART_Baudrate_76800,
+  USART_Baudrate_115200,
+  USART_Baudrate_230400
+} USART_Baudrate_t;
 
 /*******************| Global variables |*******************************/
 
 /*******************| Function prototypes |****************************/
-void USART_init(uint16_t baudrate);
+void USART_init(USART_Baudrate_t baudrate);
 
 #endif
 /** @}*/
